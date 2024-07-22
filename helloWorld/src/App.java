9a6bae75f8ce4baabf50139be89151cb39c54e29
david <davidvictorcontato7@gmail.com>
2024-07-22 14:20:46 -0300

    primeiro codigo

---
+++ helloWorld/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x = 8;
+        int z = 8;
+        System.out.println(("o valor total da compra é de ") + (x + z));
+    }
+}
