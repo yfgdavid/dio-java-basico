e2c9ae2dd1a06fa3bfa33829e39bd21e67ae602e
yfgdavid <davidvictorcontato7@gmail.com>
2024-07-26 11:45:02 -0300

    primeiro metodo

--- FirstClass/src/MinhaClasse.java
+++ FirstClass/src/MinhaClasse.java
@@ -1,8 +1,16 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.print("ola");
+        String primeiroNome = "David";
+        String segundoNome = "Victor";
 
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
     }
 
+    public static String nomeCompleto(String primeiroNome, String SegundoNome) {
+        return ("Resultado do metodo = ") + primeiroNome.concat(" ").concat(SegundoNome);
+
+    }
 }
